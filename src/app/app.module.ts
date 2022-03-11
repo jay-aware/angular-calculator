1b51f62cde44830659f70910bf1708e465064392
jay-aware <jay.aware.02@gmail.com>
2022-03-11 22:15:36 +0530

    Add Calculator Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from  './calculator/calculator.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, CalculatorComponent ],
+  bootstrap:    [ AppComponent, CalculatorComponent ]
 })
 export class AppModule { }

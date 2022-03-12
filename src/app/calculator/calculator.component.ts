c90e83025f7b99ec58803d89d9ff76745c2aed4e
jay-aware <jay.aware.02@gmail.com>
2022-03-12 20:50:29 +0530

    add basic frontend code

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+
+  input:string = "5+65";
+  result:string = "566";
+
+
   constructor() { }
 
   ngOnInit() {

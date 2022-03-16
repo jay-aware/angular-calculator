83e6886a73e03556d8aaf5bba5f791aedbcf841b
jay-aware <jay.aware.02@gmail.com>
2022-03-16 14:09:31 +0530

    Add basic styling

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -11,6 +11,11 @@ export class CalculatorComponent implements OnInit {
   input:string = "5+65";
   result:string = "566";
 
+  allClear() {
+    this.input = "";
+    this.result = "";
+  }
+
 
   constructor() { }
 

723e5e09234052cc99aeda18a5a289a3f2960d9f
jay-aware <jay.aware.02@gmail.com>
2022-03-16 15:16:16 +0530

    add number input code

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,23 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent implements OnInit {
-
-
-  input:string = "5+65";
-  result:string = "566";
+  input: string = '';
+  result: string = '566';
 
   allClear() {
-    this.input = "";
-    this.result = "";
+    this.input = '';
+    this.result = '';
   }
 
-
-  constructor() { }
-
-  ngOnInit() {
+  clear() {
+    if (this.input != '') {
+      this.input = this.input.substr(0, this.input.length - 1);
+    }
   }
 
+  pressNum(num: string) {
+    if (num == "0") {
+      if(this.input==""){
+        return;
+      }
+
+      const prevKey = this.input[this.input.length-1];
+      if (prevKey === '/' || prevKey === '*' || prevKey === "-" || prevKey === "+") {
+        return;
+      }
+    }
+
+    this.input = this.input + num;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
